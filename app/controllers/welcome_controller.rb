84c043134db95354995296770a5fbee60d654610
Tarek Amr <gr33ndata@yahoo.com>
2014-11-17 14:10:47 +0200

    Adding contact us and privacy policy pages

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,10 @@
 class WelcomeController < ApplicationController
   def about
   end
+
+  def contact
+  end
+
+  def privacy
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
   get 'about' => 'welcome#about'
+  get 'contact' => 'welcome#contact'
+  get 'privacy' => 'welcome#privacy'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
